ae710f3445c093e1b85c9aa5cf3f89a277544479
itayd100 <itayd100@gmail.com>
2019-01-06 16:15:35 +0100

    mergeHex init python kivy

---
+++ main.py
@@ -0,0 +1,96 @@
+import kivy
+kivy.require("1.10.0")
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+import os
+
+class mergehexGridLayout(GridLayout):
+
+    # Function called when equals is pressed
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                # Solve formula and display it in entry
+                # which is pointed at by display
+                self.display.text = str(eval(calculation))
+            except Exception:
+                self.display.text = "Error"
+
+    '''
+    def open(self, path, filename):
+        #with open(os.path.join(path, filename[0])) as f:
+        #    print (f.read())
+        try:
+            #print (path)
+            print (filename[0])
+        except:
+            pass
+    '''
+
+    def selected(self, filename):
+        print ("selected: %s" % filename[0])
+
+    def selectBoot(self, path, filename):
+        print ("select boot")
+        try:
+            print (filename[0])
+            filenameBoot = filename[0]
+            print ("chose bootloader")
+        except:
+            pass
+
+    def selectApp(self, path, filename):
+        print ("select App")
+        try:
+            print (filename[0])
+            print (filename)
+            filenameApp = filename[0]
+            print ("chose App")
+        except:
+            pass
+
+    def selectDes(self, path, filename):
+        print ("select Des")
+        try:
+            print (filename[0])
+            filenameDes = path
+            print ("chose Des")
+        except:
+            pass
+
+    def merge(self, filenameBoot, filenameApp, filenameDes):
+
+        #print ("printing...")
+        #print (filenameBoot)
+        filenameBootClean = (str(filenameBoot))[2:-2]
+        print (filenameBootClean)
+        #print (filenameApp)
+        filenameAppClean = (str(filenameApp))[2:-2]
+        #print (filenameDes)
+
+        print ("srec_cat.exe " + str(filenameBootClean) + " -Intel " + str(filenameAppClean) + " -Intel " + " -o " + "merge.hex" + " -Intel")
+        os.system("srec_cat.exe " + str(filenameBootClean) + " -Intel " + str(filenameAppClean) + " -Intel " + " -o " + "merge.hex" + " -Intel")
+
+        return ("done!")
+
+
+    def printit(self, it):
+        print ("printit: ")
+        print (it)
+
+#    def selectBootloader(self, bootPath):
+#            self.bootPath =
+
+class mergehexApp(App):
+
+    filenameBoot = ''
+    filenameApp = ''
+    filenameDes = ''
+    def build(self):
+        return mergehexGridLayout()
+
+
+
+mergehexApp = mergehexApp()
+mergehexApp.run()
---
+++ main.spec
@@ -0,0 +1,36 @@
+# -*- mode: python -*-
+from kivy.deps import sdl2, glew
+
+block_cipher = None
+
+
+a = Analysis(['main.py'],
+             pathex=['D:\\python\\MergeHex'],
+             binaries=[],
+             datas=[('D:\\python\\MergeHex\\mergehex.kv', 'data')],
+             hiddenimports=['win32timezone'],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+
+
+exe = EXE(pyz,Tree('D:\\python\\MergeHex'),
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          [],
+          name='main',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          runtime_tmpdir=None,
+		  *[Tree(p) for p in (sdl2.dep_bins + glew.dep_bins)],
+          console=True )
